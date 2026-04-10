a5569691154d2f0e2220458dcbc8f000f815bcd9
Camila <basso.c985@unesc.net>
2026-04-10 15:10:46 -0300

    Formatando imagem e preço

--- index.php
+++ index.php
@@ -60,17 +60,17 @@
                 <?php foreach ($dadosCafe as $cafe):?>
                 <div class="container-produto">
                         <div class="container-foto">
-                            <img src="<?= "img/" . $cafe->getImagem() ?>">
+                            <img src="<?= $cafe->getImagemFormatada() ?>">
                         </div>
                         <p><?= $cafe->getNome() ?></p>
                         <p><?= $cafe->getDescricao()?></p>
-                        <p><?="R$ " . number_format($cafe->getPreco(), 2) ?></p>
+                        <p><?= $cafe->getPrecoFormatado() ?></p>
                     </div>
                 <?php endforeach?>
             </div>
         </section>
         <section class="container-almoco">
-            <div class="container-almoa['ico-titulo">
+            <div class="container-almoco-titulo">
                 <h3>Opções para o Almoço</h3>
                 <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
@@ -78,11 +78,11 @@
                 <?php foreach($dadosAlmoco as $almoco):?>
                     <div class="container-produto">
                         <div class="container-foto">
-                            <img src="<?= "img/" . $almoco->getImagem() ?>">
+                            <img src="<?= $almoco->getImagemFormatada()?>">
                         </div>
                         <p><?= $almoco->getNome() ?></p>
                         <p><?= $almoco->getDescricao() ?></p>
-                        <p><?="R$ " . number_format($almoco->getPreco(), 2) ?></p>
+                        <p><?= $almoco->getPrecoFormatado() ?></p>
                     </div>
                 <?php endforeach?>
             </div>
