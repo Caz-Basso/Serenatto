b5f9b7a1fba806b09dd69b6a90d3a3894dec0b9d
Camila <basso.c985@unesc.net>
2026-04-10 17:13:36 -0300

    testando função delete

--- admin.php
+++ admin.php
@@ -57,7 +57,8 @@
         <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
         <td>
           <form>
-            <input type="button" class="botao-excluir" value="Excluir">
+            <input type="hidden" name="id" value="<?= $produto->getId() ?>">
+            <input type="submit" class="botao-excluir" value="Excluir">
           </form>
         </td>
       </tr>
